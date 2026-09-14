861d22a9924defee268872c3e31e87c74542b7fe
SUDHAN <sudzzselvam29@gmail.com>
2026-09-14 11:17:35 +0530

    dd

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import Overlay from "@/components/Overlay";
 export default function Hero() {
   return (
     <div className="relative" id="home">
-      <ScrollyVideo src="/upscaled-video-8k-1.mp4">
+      <ScrollyVideo src="/hero-video-scrub-ready.mp4">
         {(progress: any) => <Overlay scrollYProgress={progress} />}
       </ScrollyVideo>
     </div>
