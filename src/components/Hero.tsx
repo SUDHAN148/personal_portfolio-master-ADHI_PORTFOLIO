f7f5b9122d0fe72a5a18c9edad8e058bcc8f6f3b
SUDHAN <sudzzselvam29@gmail.com>
2026-09-14 11:33:41 +0530

    For mobile and desktop

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,8 @@ import Overlay from "@/components/Overlay";
 export default function Hero() {
   return (
     <div className="relative" id="home">
-      <ScrollyVideo src="/hero-video-4k.mp4">
+      <ScrollyVideo src="/hero-video-desktop-scrub.mp4"
+  srcMobile="/hero-video-mobile-scrub.mp4">
         {(progress: any) => <Overlay scrollYProgress={progress} />}
       </ScrollyVideo>
     </div>
